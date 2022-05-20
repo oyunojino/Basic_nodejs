f560db493b3fe784d6dff170e74417a51615c3d4
oyunojino <yjoh@kci.co.kr>
2022-05-20 17:14:24 +0900

    2장_자바스크립트 리마인드_변수, 호이스팅

---
+++ sample01.js
@@ -0,0 +1,8 @@
+console.log(puppy);
+// <--- 결 과 --->
+// VM40:1 Uncaught ReferenceError: puppy is not defined at <anonymous>:1:13
+
+var puppy = "cute";
+console.log(puppy);
+// <--- 결 과 --->
+// cute
---
+++ sample02-1.js
@@ -0,0 +1,5 @@
+let dog;
+dog = "happy";
+console.log(dog);
+// let dog = "happy";
+// 이미 정의 되어 있어서 안됨
